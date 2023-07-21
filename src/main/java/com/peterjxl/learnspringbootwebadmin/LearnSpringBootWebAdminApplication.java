f66f87d2353e8b9575c305ca3144567e212d8b8e
peterjxl <peterjxl@qq.com>
2023-07-21 22:16:26 +0800

    初始化项目及静态资源

---
+++ src/main/java/com/peterjxl/learnspringbootwebadmin/LearnSpringBootWebAdminApplication.java
@@ -0,0 +1,13 @@
+package com.peterjxl.learnspringbootwebadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBootWebAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringBootWebAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/peterjxl/learnspringbootwebadmin/LearnSpringBootWebAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peterjxl.learnspringbootwebadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootWebAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
