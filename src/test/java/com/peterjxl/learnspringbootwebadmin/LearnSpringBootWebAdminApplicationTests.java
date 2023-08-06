86df92bbb4846cea6ea69cc07e37ad5102520b0a
peterjxl <peterjxl@qq.com>
2023-08-06 21:52:23 +0800

    数据开发场景

--- src/test/java/com/peterjxl/learnspringbootwebadmin/LearnSpringBootWebAdminApplicationTests.java
+++ src/test/java/com/peterjxl/learnspringbootwebadmin/LearnSpringBootWebAdminApplicationTests.java
@@ -1,13 +1,22 @@
 package com.peterjxl.learnspringbootwebadmin;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+@Slf4j
 @SpringBootTest
 class LearnSpringBootWebAdminApplicationTests {
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @Test
     void contextLoads() {
+        Long aLong = jdbcTemplate.queryForObject("select count(*) from students", Long.class);
+        log.info("记录总数：{}", aLong);
     }
 
 }
