74cac1a6a926f18fd66f2651f73cd61907186993
peterjxl <peterjxl@qq.com>
2023-08-15 11:42:04 +0800

    添加删除戒

--- src/main/java/com/peterjxl/learnspringbootwebadmin/controller/TableController.java
+++ src/main/java/com/peterjxl/learnspringbootwebadmin/controller/TableController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -51,5 +53,15 @@ public class TableController {
     @GetMapping("/editable_table")
     public String editable_table(){
         return "table/editable_table";
+    }
+
+
+    @GetMapping("/user/delete/{id}")
+    public String deleteUser(@PathVariable("id") Long id,
+                             @RequestParam(value = "pn", defaultValue = "1") Integer pn,
+                             RedirectAttributes ra){
+        userService.removeById(id);
+        ra.addAttribute("pn", pn);
+        return "redirect:/dynamic_table";  // 删除后回到当前页
     }
 }
